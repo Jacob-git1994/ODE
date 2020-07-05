5046fa5a0d851b91f0f72270595737a47b00de5f
Jacob Dresher <JacobDresher@gmail.com>
2020-07-05 19:11:38 -0400

    Update Main

--- main.cpp
+++ main.cpp
@@ -8,9 +8,11 @@ using namespace ode;
 
 int main()
 {
-	OdeParameters params(.00001, {.0000005,.00003},odeInterface::OdeSolvers::IMPLICT_EULER,10);
+	OdeParameters params1(.00001, {.0000005,.00003},odeInterface::OdeSolvers::IMPLICT_EULER,10);
+
+	OdeParameters params = params1;
+
 
 	std::cout << params.getInitalDeltatime() << "\n" << params.getErrorBound()[0] << " " << params.getErrorBound()[1] << "\n" << static_cast<int>(params.getPreferredMethod()) << "\n" << params.getRecordFreq() << "\n";
 	std::cout << params.checkParams();
-	return 0;
 }
